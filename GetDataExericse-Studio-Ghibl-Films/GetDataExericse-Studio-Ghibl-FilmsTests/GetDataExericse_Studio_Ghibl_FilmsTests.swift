7d3cd6e0c65555adb9c8e77171afc643de122e8d
Xianxian Chen <xianxianchen@ac.c4q.nyc>
2017-11-27 17:13:18 -0500

    fixed errors from file move

---
+++ GetDataExericse-Studio-Ghibl-Films/GetDataExericse-Studio-Ghibl-FilmsTests/GetDataExericse_Studio_Ghibl_FilmsTests.swift
@@ -0,0 +1,36 @@
+//
+//  GetDataExericse_Studio_Ghibl_FilmsTests.swift
+//  GetDataExericse-Studio-Ghibl-FilmsTests
+//
+//  Created by C4Q on 11/27/17.
+//  Copyright © 2017 C4Q. All rights reserved.
+//
+
+import XCTest
+@testable import GetDataExericse_Studio_Ghibl_Films
+
+class GetDataExericse_Studio_Ghibl_FilmsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
